af422a731be4cd3a2f5d99ae1640fed8f7665594
Ваше Имя <you@example.com>
2025-07-30 15:38:02 +0500

    sql fix

--- CardService/internal/migrations/init.sql
+++ CardService/internal/migrations/init.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS cards (
     id BIGSERIAL PRIMARY KEY,
     account_id UUID NOT NULL,
     encrypted_pan BYTEA NOT NULL,
-    hashed_pan BYTEA NOT NULL
+    hashed_pan BYTEA NOT NULL,
     expiry_month INT NOT NULL CHECK (expiry_month >= 1 AND expiry_month <= 12),
     expiry_year INT NOT NULL,
     -- encrypted_cvv VARCHAR(255) NOT NULL,
