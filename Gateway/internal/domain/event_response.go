7f5397bad3cbde6d95ffbcf38781b1a62039de94
Ваше Имя <you@example.com>
2025-08-15 11:57:07 +0500

    added data to responce

--- Gateway/internal/domain/event_response.go
+++ Gateway/internal/domain/event_response.go
@@ -3,10 +3,15 @@ package domain
 import "github.com/google/uuid"
 
 type EventResponse struct {
-	id uuid.UUID
+	id   uuid.UUID
+	data []byte
 	EventStatus
 }
 
 func (e *EventResponse) ID() uuid.UUID {
 	return e.id
+}
+
+func (e *EventResponse) Data() []byte {
+	return e.data
 }
